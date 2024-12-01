2ad25027a17494280d17f1bdb28956f671c68b8c
shahnawazkhan11 <shahnawaz04khan@gmail.com>
2024-12-01 15:27:11 +0530

    nativewind added

---
+++ mobile/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function TabOneScreen() {
+  return (
+    <View className="bg-red-700 flex-1 justify-center items-center">
+      <Text className="text-blue-400 text-lg">Hello, Tailwind CSS!</Text>
+    </View>
+  );
+}
